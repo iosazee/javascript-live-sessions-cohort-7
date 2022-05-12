d7917520706d5bd9ee560b5938f8597d15828329
Connor Todd <connortodd@connors-mbp.lan>
2022-05-12 17:44:51 +0100

    lesson plan completed for conditional statements

---
+++ index.js
@@ -0,0 +1,7 @@
+// Topics to cover
+// Introduce conditional statements and pseudocode
+// use || as well as &&
+// more than, less than operators for numbers
+// boolean conditionals
+// ternary operator
+// switch statements
