204339abad213e29a3fe848c8fd62b56e3e77abd
Connor Todd <connortodd@connors-mbp.lan>
2022-05-19 17:47:39 +0100

    lesson plan for arrays loops and functions

---
+++ arrays-loops-functions.js
@@ -0,0 +1,39 @@
+// Arrays
+
+// create a cities array
+// display the length of the cities array
+// show index of an element within the array
+// push a new element to the array
+// pop that new element off the end of the array
+// retreive a value using the index from the array
+// change the value of an element within the array
+
+
+// Loops
+// for loops
+// e.g create a loop which runs 5 times printing out the iterator and a message of what iteration the loop is at
+// e.g. loop over array of cities and print out a welcome message for each city
+// level 2. add a conditional to check if the city is Lisbon and print a different message
+
+// while loops
+// e.g. loop 5 times each time incrementing
+// show drawbacks of while loops
+
+// Functions
+// console.log inside functions
+// e.g. create a function which displays a welcome message sending in a name parameter
+
+// return value from function and wrap in console.log
+// e.g. change the function to console.log the return value of the function
+// e.g. use the return value of the function and store it in a variable to use it later on
+// create a favourite food function that receives a favourite food parameter it will return a message stating what the favourite food is. Store this in a variable
+// print out the result of the favourite food variable and the welcome message variable.
+
+// combine function and conditionals
+// add a conditional inside the favourite food condition of your choice
+
+// combine function, loop and array
+// e.g create a cityWelcomeMessage function which uses the cities array and prints out a welcome message for each city
+
+// combine function, loop, array and conditional
+// e.g. add an if statement to print out a different message for the city of your choice.
